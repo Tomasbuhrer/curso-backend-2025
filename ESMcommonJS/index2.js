74dda781845d065b323b3055a5d3c1abd064b7b0
tomas emilio buhrer <tomasemiliobuhrer@hotmail.com>
2025-12-16 12:20:12 -0300

    practica esm

---
+++ ESMcommonJS/index2.js
@@ -0,0 +1,4 @@
+import { sumar, restar } from "./math2.js";
+
+console.log("2 + 3 = ", sumar(2,3));
+console.log("3 - 2 =", restar(3,2));
---
+++ ESMcommonJS/math2.js
@@ -0,0 +1,12 @@
+
+/*
+Funciones modularizadas encargadas de sumar y restar datos
+*/
+
+export function sumar (a,b){
+    return a + b;
+}
+
+export function restar(a,b){
+    return a - b;
+}
